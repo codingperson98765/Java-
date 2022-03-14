fbe8bac8683e714b5b6d875144a947529a2f9f82
codingperson98765 <82063965+codingperson98765@users.noreply.github.com>
2022-03-13 20:55:39 -0600

    Update Login.java

--- Login.java
+++ Login.java
@@ -11,9 +11,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Component;
 /*Ahmedh Shamsudeen
-UCID 30121541
-A5 Version 1.0
-Tutorial 7
 Purpose: Handles all aspects of the Login Window
 Contains; userLogin() method which handles all the action events when the use tries to login. 
 Features;
